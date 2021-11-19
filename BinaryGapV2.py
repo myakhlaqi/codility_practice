8498fee5e162baf0c713419048e0d2c820eb6eb2
yahya <csmaster90@hotmail.com>
2021-11-19 12:57:14 +0800

    Add a new version of BinaryGap.py
    Simple code using re module and bin() method for solving the problem.

---
+++ BinaryGapV2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import re
+
+def solution(N):
+    #binary_n=bin(N)[2:]
+    #binary_n=format(N,"b")
+    binary_n="{0:b}".format(N)
+    rx_pattern=r"010"
+    binary_n=re.sub(rx_pattern,"0110",binary_n)
+    result=re.findall(r"10+1",binary_n)
+    #print(max([len(x)-2 for x in result]))
+    return max([x.count("0") for x in result],default=0)
+
+print(solution(0))
+    
--- test_BinaryGap.py
+++ test_BinaryGap.py
@@ -1,18 +1,18 @@
 import unittest
-import BinaryGap
+import BinaryGapV2
 
 
 class TestBinaryGap(unittest.TestCase):
 
     def test_solution(self):
-        self.assertEqual(BinaryGap.solution(529),4,"Passed")
-        self.assertEqual(BinaryGap.solution(9),2,"Passed")
-        self.assertEqual(BinaryGap.solution(20),1,"Passed")
-        self.assertEqual(BinaryGap.solution(5),1,"Passed")
-        self.assertEqual(BinaryGap.solution(0),0,"Passed")
-        self.assertEqual(BinaryGap.solution(15),0,"Passed")
-        self.assertEqual(BinaryGap.solution(1041),5,"Passed")
-        self.assertEqual(BinaryGap.solution(32),0,"Passed")
+        self.assertEqual(BinaryGapV2.solution(529),4,"Passed")
+        self.assertEqual(BinaryGapV2.solution(9),2,"Passed")
+        self.assertEqual(BinaryGapV2.solution(20),1,"Passed")
+        self.assertEqual(BinaryGapV2.solution(5),1,"Passed")
+        self.assertEqual(BinaryGapV2.solution(0),0,"Passed")
+        self.assertEqual(BinaryGapV2.solution(15),0,"Passed")
+        self.assertEqual(BinaryGapV2.solution(1041),5,"Passed")
+        self.assertEqual(BinaryGapV2.solution(32),0,"Passed")
         
         
 
