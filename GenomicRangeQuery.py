dc3f3ae7f9d81e7945040aa890047661fd3568fe
yahya <csmaster90@hotmail.com>
2021-11-23 02:03:01 +0800

    Solve the GenomicRangeQuery in O(N)

--- GenomicRangeQuery.py
+++ GenomicRangeQuery.py
@@ -18,4 +18,31 @@ def solution(S, P, Q):
         
     return result
 
-print(solution("CAGCCTA",[2,5,0],[4,5,6]))
+
+def solution1(S, P, Q):
+    n=len(S)
+    m=len(P)
+    dna_matrix=[[0 for col in range(n+1)] for row in range(3)]
+    
+    for i,ch in enumerate(S):
+        dna_matrix[0][i+1]=dna_matrix[0][i]+( ch =='A')
+        dna_matrix[1][i+1]=dna_matrix[1][i]+( ch =='C')
+        dna_matrix[2][i+1]=dna_matrix[2][i]+( ch =='G')
+    #print("dna_matrix", dna_matrix)
+    
+    result=[]
+    for i in range(m):
+        start=P[i]
+        end=Q[i]+1
+        if(dna_matrix[0][end]-dna_matrix[0][start]>0):
+            result.append(1)
+        elif (dna_matrix[1][end]-dna_matrix[1][start]>0):
+            result.append(2)
+        elif (dna_matrix[2][end]-dna_matrix[2][start]>0):
+            result.append(3)
+        else:
+            result.append(4)
+
+    return result
+
+print(solution1("CAGCCTA",[2,5,0],[4,5,6]))
