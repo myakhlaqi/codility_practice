839c017935ed04f7cf3e7725d4e6f1367f9ba9c6
yahya <csmaster90@hotmail.com>
2021-11-24 20:04:23 +0800

    Add improved version of solution O(N*Log(N))

--- Triangle.py
+++ Triangle.py
@@ -9,7 +9,6 @@ def solution(A):
         for j in range(i+1,n-1):
             for k in range(j+1,n):
                 P,Q,R=A[i],A[j],A[k]
-                
                 if(P+Q>R and P+R>Q and Q+R>P):
                     return 1
     
@@ -17,6 +16,18 @@ def solution(A):
     
     return 0
 
-#A=[10,2,5,1,8,20]
-A=[10,50,5,1]
-print(solution(A))
+def solution2(A):
+    n=len(A)
+    A.sort()
+    for i in range(n-2):
+        P,Q,R=A[i],A[i+1],A[i+2]
+        if(P+Q>R and P+R>Q and Q+R>P):
+            return 1
+        
+    
+    return 0
+
+
+A=[10,2,5,1,8,20]
+#A=[10,50,5,1]
+print(solution2(A))
