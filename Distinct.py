ea19685423093a4d2e3fd5f78a0722b9f7ac8ece
yahya <csmaster90@hotmail.com>
2021-11-24 12:01:35 +0800

    Solve the Distinct problem in O(N)
     using counting sort

---
+++ Distinct.py
@@ -0,0 +1,6 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+from collections import Counter
+def solution(A):
+    dic_a=dict(Counter(A))
+    return len(dic_a)
