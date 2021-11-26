c5d240f46da2136f6dbd0d71a96d9cdb9b1eb241
yahya <csmaster90@hotmail.com>
2021-11-26 22:25:01 +0800

    Solve the Codility challenges in O(N)
    Awarded 47th Codility golden award

--- Challenge1.py
+++ Challenge1.py
@@ -23,35 +23,41 @@ def solution(S):
             next=B[i+1]
             prev=B[i-1]
             pattern=prev[0]+"?"+next[0]
+            replace=""
             if(pattern in ["a?a","b?b"]):
-                replace=""
                 if(B[i][1]%2==0):
-                    if(next[1]>=prev[1]):
-                        replace=prev[0]+opposite(prev[0])
+                    if(next[1]>prev[1]):
+                        replace=(prev[0]+opposite(prev[0]))*(B[i][1]//2)
                         B[i-1][1]+=1  
-                    else: 
-                        replace=opposite(prev[0])+prev[0]
+                    elif next[1]<prev[1]:
+                        replace=(opposite(prev[0])+prev[0])*(B[i][1]//2)
                         B[i+1][1]+=1
-                    C[i]=(replace*(B[i][1]//2),1)
+                    else:
+                        replace=opposite(prev[0])*2+(opposite(prev[0])+prev[0])*((B[i][1]//2)-1)
+                        B[i][0]="="
+                        B[i][1]=2
                 else:
-                    replace=prev[0]+opposite(prev[0])
-                    C[i]=(opposite(prev[0])+(replace*(B[i][1]//2)),1)
+                    replace=opposite(prev[0])+( (prev[0] + opposite(prev[0]) ) * (B[i][1]//2) )
             else:
                 if(B[i][1]%2==1):
                     if(next[1]>prev[1]):
-                        replace=next[0]+opposite(next[0])
-                        C[i]=(opposite(next[0])+(replace*(B[i][1]//2)),1)
+                        replace=opposite(next[0])+((next[0]+opposite(next[0]) )*(B[i][1]//2))
                         B[i-1][1]+=1
                     elif next[1]<prev[1]:
-                        replace=prev[0]+opposite(prev[0])
-                        C[i]=(opposite(prev[0])+(replace*(B[i][1]//2)),1)
+                        replace=opposite(prev[0])+(( prev[0]+opposite(prev[0]) )*(B[i][1]//2) )
                         B[i+1][1]+=1
-
+                    else:
+                        if(B[i][1]==1):
+                            B[i-1][1]+=1
+                            C[i]=prev[0]
+                        else:
+                            replace=opposite(prev[0])+((opposite(prev[0])+prev[0] )*(B[i][1]//2))
+                            B[i][0]="="
+                            B[i][1]=2
                 else:
-                    replace=opposite(prev[0])+prev[0]
-                    C[i]=(replace*(B[i][1]//2),1)
+                    replace=(opposite(prev[0])+prev[0])*(B[i][1]//2)
             #print("replace", replace)
-            #print("C", C)
+            C[i]=(replace,1)
         # print(B)
     print("".join([x[0]*x[1] for x in C]))
 
@@ -59,11 +65,48 @@ def solution(S):
     return max([x[1] for x in B if x[0]!="?"],default=1)
 
 
+def solution2(S):
+
+    result=re.finditer(r'(a+|\?+|b+)',S)
+    B=[]
+    for m in result:
+        B.append([m.group()[0],m.end()-m.start()])
+        
+    for i in range(1,len(B)-1):
+        if(B[i][0]=="?"):
+            next=B[i+1]
+            prev=B[i-1]
+            pattern=prev[0]+"?"+next[0]
+            if(pattern in ["a?a","b?b"]):
+                if(next[1]>prev[1]):
+                    B[i-1][1]+=1  
+                elif next[1]<prev[1]:
+                    B[i+1][1]+=1
+                else:
+                    B[i][0]="="
+                    B[i][1]=2
+            else:
+                if(B[i][1]%2==1):
+                    if(next[1]>prev[1]):
+                        B[i-1][1]+=1
+                    elif next[1]<prev[1]:
+                        B[i+1][1]+=1
+                    else:
+                        if(B[i][1]==1):
+                            B[i-1][1]+=1
+                        else:
+                            B[i][0]="="
+                            B[i][1]=2
+
+    return max([x[1] for x in B if x[0]!="?"],default=1)
+
+
+
 s1 = "????aaa?bb??aa???bbb??aa?aa??b?bb??"
 s2 = "aa??bbb"
 s3 = "aa?????bbb"
 s4 = "aa?b?aa"
-s5= "aaa???bbb"
+s5= "bb??bb"
 
 print(solution(s5))
 # print(solution(s2))
