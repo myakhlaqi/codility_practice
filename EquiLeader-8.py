4dc1edafcb246742f57bee9a1ac0ea97a2b2e7ad
yahya <csmaster90@hotmail.com>
2021-11-28 01:17:03 +0800

    Solve EquiLeader problem in O(N**2)

---
+++ EquiLeader-8.py
@@ -0,0 +1,45 @@
+#Find the index S such that the leaders of the sequences A[0], A[1], ..., A[S] and A[S + 1], A[S + 2], ..., A[N - 1] are the same.
+from collections import Counter
+from collections import deque
+def leader(A):
+    stack=deque()
+    n=len(A)
+    leader=-1
+    counter=0
+    leader_index=-1
+    for i in range(n):
+        if(len(stack)==0):
+            stack.append(A[i])
+        else:
+            if(stack[-1]!=A[i]):
+                stack.pop()
+            else:
+                stack.append(A[i])
+    
+    if(len(stack)>=1):
+        leader=stack[0]
+    for i in range(n):
+        if(A[i]==leader):
+            counter+=1
+            leader_index=i
+    if(counter>n//2):
+        return leader,leader_index
+    else:
+        return (-1,-1)
+
+def solution(A):
+    n=len(A)
+    counter=0
+    for i in range(n):
+        L1,i1=leader(A[0:i+1])
+        L2,i2=leader(A[i+1:n])
+        print("{},{}=> ({},{}) and ({},{})".format(A[0:i+1],A[i+1:n],L1,i1,L2,i2))
+        
+        if(i1!=-1 and i2!=-1 and L1==L2):
+            counter+=1
+            print("find")
+    return counter
+#A=[4,3,4,4,4,2]
+A=[-1, 4, -1, 3, -1]
+print(solution(A))
+
