51d8827e183cb063b352f3c22b8780f316192e48
yahya <csmaster90@hotmail.com>
2021-11-27 15:07:03 +0800

    solve Dominator problem in O(N*log(N))
    I used Counter class for counting elements.

---
+++ Dominator-8.py
@@ -0,0 +1,21 @@
+# Find an index of an array such that its value occurs at more than half of indices in the array.
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+from collections import Counter
+import sys
+def solution(A):
+    dic_A=dict(Counter(A))
+    max=-sys.maxsize
+    dominant_ixs=0
+    B=[]
+    for key in dic_A:
+        if dic_A[key]>max:
+            max=dic_A[key]
+            dominant_ixs=key
+    if(max>=(len(A)//2+1)):
+        B=[i for i in range(len(A)) if A[i]==dominant_ixs]
+        return B[0]
+    return -1
+
+
+print(solution([3, 4, 3, 2, 3, -1, 3, 3]))
