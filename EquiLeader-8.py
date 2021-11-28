0a30d96dfa77043cb9a88a30c018bfd8e23edbc0
yahya <csmaster90@hotmail.com>
2021-11-29 00:07:17 +0800

    Stable version

--- EquiLeader-8.py
+++ EquiLeader-8.py
@@ -1,21 +1,39 @@
 #Find the index S such that the leaders of the sequences A[0], A[1], ..., A[S] and A[S + 1], A[S + 2], ..., A[N - 1] are the same.
 from collections import deque
-import random
 
-def solution(A):
-    leader_ix=leader(A)
-    equi_leaders = 0
-    leader_count_so_far = 0
+def solution3(A):
+    """ O(N) and 100 precentage of correctness"""
+    stack=deque()
     n=len(A)
-    for index in range(n):
-        if A[index] == A[leader_ix]:
-            leader_count_so_far += 1
-        if leader_count_so_far > (index+1)//2 and
-           leader_count-leader_count_so_far > (n-index-1)//2:
-            # Both the head and tail have leaders of the same value
-            # as "leader"
-            equi_leaders += 1
-    return equi_leaders
+    leader=None
+    leader_count=0
+    for i in range(n):
+        if(len(stack)==0):
+            stack.append(A[i])
+        else:
+            if(stack[-1]!=A[i]):
+                stack.pop()
+            else:
+                stack.append(A[i])
+    if(len(stack)>0):
+        leader=stack[0]
+    for i in range(n):
+        if(A[i]==leader):
+            leader_count+=1
+    if(leader_count<=n//2):
+        return 0
+    else:
+        equi_leaders = 0
+        leader_count_so_far = 0
+        for index in range(n):
+            if A[index] == leader:
+                leader_count_so_far += 1
+            if leader_count_so_far > (index+1)//2 and \
+            leader_count-leader_count_so_far > (n-index-1)//2:
+                # Both the head and tail have leaders of the same value
+                # as "leader"
+                equi_leaders += 1
+        return equi_leaders
 
 def leader_all(A):
     stack=deque()
@@ -42,11 +60,11 @@ def solution2(A):
     """O(N) but error in one case A=[4,1,2,3,4,4,4] expected 0 get 2"""
     n=len(A)
     left_leaders=leader_all(A[:-1])
-    print("left_leaders", left_leaders)
+    #print("left_leaders", left_leaders)
     B=A[::-1]
     right_leaders=leader_all(B[:-1])
     right_leaders=right_leaders[::-1]
-    print("right_leaders", right_leaders)
+    #print("right_leaders", right_leaders)
     counter=0
     for i in range(len(left_leaders)):
         if(right_leaders[i]==left_leaders[i] and right_leaders[i]!=None and left_leaders[i]!=None):
@@ -86,7 +104,7 @@ def solution1(A):
         right_leader_index=leader(A[i+1:n])
         
         if(left_leader_index!=None and right_leader_index!=None and A[left_leader_index]==A[right_leader_index]):
-            print("{},{}=> ({},{}) and ({},{})".format(A[0:i+1],A[i+1:n],A[left_leader_index],left_leader_index,A[right_leader_index],right_leader_index))
+            #print("{},{}=> ({},{}) and ({},{})".format(A[0:i+1],A[i+1:n],A[left_leader_index],left_leader_index,A[right_leader_index],right_leader_index))
             counter+=1
     return counter
 #A=[0]*10
@@ -94,8 +112,10 @@ def solution1(A):
 #A=[-1, 4, -1, 3, -1]
 #A=random.sample(range(-10000000000,1000000000),300)
 #A=[-999999999]*300
-A=[4,1,2,3,4,4,4]
-#A=[4,3,4,4,4,2]
+#A=[4,1,2,3,4,4,4]
+A=[4,3,4,4,4,2]
 print("leader :",leader(A))
-print(solution2(A))
+
 print(solution1(A))
+print(solution2(A))
+print(solution3(A))
