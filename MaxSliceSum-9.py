1c15e7ebf38225b98511ef9ddc85458f9930154c
yahya <csmaster90@hotmail.com>
2021-11-29 01:51:33 +0800

    solve the problem in O(N)

---
+++ MaxSliceSum-9.py
@@ -0,0 +1,14 @@
+#Find a maximum sum of a compact subsequence of array elements.
+import sys
+def solution(A):
+    max_ending = max_slice = -sys.maxsize
+    if(len(A)==1):
+        return A[0]
+    else:
+        for a in A:
+            max_ending =max(a,max_ending + a)
+            max_slice = max(max_slice, max_ending)
+        return max_slice
+
+#print(solution([-10,-20]))
+print(solution([3,2,-6,4,0]))
