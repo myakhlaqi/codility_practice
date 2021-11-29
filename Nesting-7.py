442ed6863a88429745b28262115c6ef85fe8e5f8
yahya <csmaster90@hotmail.com>
2021-11-30 00:13:55 +0800

    Solve Nesting problem in O(N)

---
+++ Nesting-7.py
@@ -0,0 +1,28 @@
+from collections import deque
+def get_open(ch):
+    if(ch=="}"):
+        return "{"
+    elif(ch==")"):
+        return "("
+    elif(ch=="]"):
+        return "["
+    
+def solution(S):
+    q=deque()
+    for i in S:
+        if i in "{[(":
+            q.append(i)
+        else:
+            if(len(q)==0):
+                return 0
+            if(q[-1]==get_open(i)):
+                q.pop()
+            else:
+                return 0
+    
+    if(len(q)!=0):
+        return 0
+    return 1
+
+print(solution("(()(())())"))
+print(solution("())"))
