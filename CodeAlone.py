20844556cf8597f89e70f90b4967c4bb7df25d18
yahya <csmaster90@hotmail.com>
2021-12-23 13:00:21 +0800

    Secend version of the code with error on ababab

--- CodeAlone.py
+++ CodeAlone.py
@@ -1,6 +1,8 @@
 from collections import Counter
 import re
 import copy
+import sys
+from typing import MappingView
 
 def solution(S):
     S_counter=dict(Counter(S))
@@ -22,30 +24,46 @@ def solution(S):
         return 0
     else:
         a_max_length=max([x[1] for x in B if x[0]=="a"])
-        a_max_index=B.index(['a',a_max_length])
-        
         b_max_length=max([x[1] for x in B if x[0]=="b"])
-        b_max_index=B.index(['b',b_max_length])
-
+        
+        a_sum=0
+        pre,main,next=0,0,0
+        min_total=sys.maxsize
         if(a_max_length<3):
-            while(a_max_length<3):
-                a_replace_ix=-1
-                for i in range(a_max_index+1,len(B)):
-                    if(B[i][0]=="a" and B[i][1]>0):
-                        a_replace_ix=i
-                        break
-                if(a_replace_ix==-1):#didn't find, so search other side
-                    for i in range(a_max_index-1,-1,-1):
-                        if(B[i][0]=="a" and B[i][1]>0):
-                            a_replace_ix=i
-                            break
-                gap_size=sum([x[1] for x in B[min(a_replace_ix,a_max_index)+1:max(a_replace_ix,a_max_index)]])
-                B[a_max_index][1]+=1
-                a_max_length+=1
-                B[a_replace_ix][1]-=1
-                swap_count+=gap_size
-                print("{}\tcount: {}\tB[{}]=>B[{}]".format("".join([x[0]*x[1] for x in B]),swap_count,a_replace_ix,a_max_index))
-                B=[x for x in B if x[1]>0]
+            for i in range(len(B)):
+                if(B[i][0]=="a"):
+                    left_a_gap= sys.maxsize if a_sum==0 else B[i-1][1]
+                    a_sum+=B[i][1]
+                    right_a_gap= sys.maxsize if a_sum==a_count else B[i+1][1]
+                    
+                    min_swap=0
+                    if(B[i][1]==2):#if aa 
+                        min_swap=min(left_a_gap,right_a_gap)
+                    else:# if just one a
+                        min_swap=(left_a_gap+right_a_gap)
+                    if(min_swap<min_total):
+                        main=i
+                        pre=None if left_a_gap==sys.maxsize else i-2
+                        next=None if right_a_gap==sys.maxsize else i+2
+                        print("{},{},{}".format(pre,main,next))
+                    min_total=min(min_swap,min_total)
+            
+            if(B[main][1]==1):
+                B[main][1]+=2
+                B[pre][1]-=1
+                B[next][1]-=1
+                swap_count+=(B[main-1][1]+B[main+1][1])
+            elif(B[main][1]==2):
+                B[main][1]+=1
+                if(pre is None and next is None):
+                    return -1
+                elif(pre is None):
+                    B[next][1]-=1
+                    swap_count+=B[main+1][1]
+                elif(next is None):
+                    B[pre][1]-=1
+                    swap_count+=B[main-1][1]
+        B=[x for x in B if x[1]>0]
         B_stack=[]
         for i in B:
             if(len(B_stack)==0):
@@ -55,38 +73,49 @@ def solution(S):
                     B_stack[-1][1]+=i[1]
                 else:
                     B_stack.append(i)
-                    
-        print("B_stack", B_stack)
         B=B_stack
         b_max_length=max([x[1] for x in B if x[0]=="b"])
-        b_max_index=B.index(['b',b_max_length])
-        print("=====================b {}=>{}=================".format(b_max_index,b_max_length))
-
+        print("=====================b=================")
+        b_sum=0
+        min_total=sys.maxsize
         if(b_max_length<3):
-            while(b_max_length<3):
-                b_replace_ix=-1
-                for i in range(b_max_index+1,len(B)):
-                    if(B[i][0]=="b" and B[i][1]>0):
-                        b_replace_ix=i
-
-                        break
-                if(b_replace_ix==-1):#didn't find, so search other side
-                    for i in range(b_max_index-1,-1,-1):
-                        if(B[i][0]=="b" and B[i][1]>0):
-                            b_replace_ix=i
-                            break
-                print("b_replace_ix", b_replace_ix)
-                gap_size=sum([x[1] for x in B[min(b_replace_ix,b_max_index)+1:max(b_replace_ix,b_max_index)]])
-                B[b_max_index][1]+=1
-                b_max_length+=1
-                B[b_replace_ix][1]-=1
-                swap_count+=gap_size
-                print("{}\tcount: {}\tB[{}]=>B[{}]".format("".join([x[0]*x[1] for x in B]),swap_count,b_replace_ix,b_max_index))
-                B=[x for x in B if x[1]>0]
-                
+            for i in range(len(B)):
+                if(B[i][0]=="b"):
+                    left_a_gap= sys.maxsize if b_sum==0 else B[i-1][1]
+                    b_sum+=B[i][1]
+                    right_a_gap= sys.maxsize if b_sum==b_count else B[i+1][1]
+                    min_swap=0
+                    if(B[i][1]==2):#if aa 
+                        min_swap=min(left_a_gap,right_a_gap)
+                    else:# if just one a
+                        min_swap=(left_a_gap+right_a_gap)
+                    if(min_swap<min_total):
+                        main=i
+                        pre=None if left_a_gap==sys.maxsize else i-2
+                        next=None if right_a_gap==sys.maxsize else i+2
+                        print("{},{},{}".format(pre,main,next))
+                    min_total=min(min_swap,min_total)
+            
+            if(B[main][1]==1):
+                B[main][1]+=2
+                B[pre][1]-=1
+                B[next][1]-=1
+                swap_count+=(B[main-1][1]+B[main+1][1])
+            elif(B[main][1]==2):
+                B[main][1]+=1
+                if(pre is None and next is None):
+                    return -1
+                elif(pre is None):
+                    B[next][1]-=1
+                    swap_count+=B[main+1][1]
+                elif(next is None):
+                    B[pre][1]-=1
+                    swap_count+=B[main-1][1]
+    print("B", B)          
     return swap_count
 
-print(solution("baabaabaabaababaab"))
+#print(solution("aabbbbababa"))
+print(solution("ababab"))
 #print(solution("abbbbaa"))
 #print(solution("bbbababaaab"))
 #print(solution("abbabb"))
