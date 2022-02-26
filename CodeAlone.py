657f56ea1a72a9afdfb03021e5e4f442885ce3b7
Mohammad Yahya Akhlaqi <yahya.akhlaghi@gmail.com>
2022-02-26 12:12:59 +0800

    Code update

--- CodeAlone.py
+++ CodeAlone.py
@@ -3,8 +3,11 @@ import re
 import copy
 import sys
 from typing import MappingView
+
+
 def reversChar(ch):
-    return "a" if ch=="b" else "b"
+    return "a" if ch == "b" else "b"
+
 
 def count_letters(S):
     result = re.finditer(r'(a+|b+)', S)
@@ -13,6 +16,7 @@ def count_letters(S):
     for m in result:
         B.append([m.group()[0], m.end()-m.start()])
     return B
+
 
 def solution(S):
     B = count_letters(S)
@@ -30,106 +34,110 @@ def solution(S):
     else:
         a_max_length = max([x[1] for x in B if x[0] == "a"])
         b_max_length = max([x[1] for x in B if x[0] == "b"])
-        current_char = "a" if a_max_length>b_max_length else "b"
+        current_char = "a" if a_max_length > b_max_length else "b"
         while(a_max_length < 3 or b_max_length < 3):
-            gapA=getGap1("a", B) 
-            gapB=getGap1("b", B) 
+            gapA = getGap1("a", B)
+            gapB = getGap1("b", B)
             if(a_max_length < 3 and b_max_length < 3):
-                if(gapA>gapB):
-                    current_char="a"
+                if(gapA > gapB):
+                    current_char = "a"
                 elif(gapB > gapA):
-                    current_char="b"
+                    current_char = "b"
             elif(a_max_length < 3):
                 current_char = "a"
             else:
                 current_char = "b"
             candidate_ix = -1
-            neighbor_ix=-1
+            neighbor_ix = -1
             min_swap_sofar = 0
             min_total_swap = sys.maxsize
             C = [x[0] for x in B]
-            ops_char_start_ix= C.index(reversChar(current_char))
-            ops_char_end_ix=len(C)-1-C[::-1].index(reversChar(current_char))
-            ops_char_gap=gapA if current_char=="b" else gapB
-            for i in range(len(B)):
-                current_neighbor_ix=-1
+            ops_char_start_ix = C.index(reversChar(current_char))
+            ops_char_end_ix = len(C)-1-C[::-1].index(reversChar(current_char))
+            ops_char_gap = gapA if current_char == "b" else gapB
+            i=0
+            while(i<len(B)):
+                current_neighbor_ix = -1
                 if(B[i][0] == current_char):
-                    if(i<=1):
-                        if(B[i][1]==2):
-                            min_swap_sofar=B[i+1][1]
-                            current_neighbor_ix=i+2
+                    if(i <= 1):
+                        if(B[i][1] == 2):
+                            min_swap_sofar = B[i+1][1]
+                            current_neighbor_ix = i+2
                         else:
-                            min_swap_sofar=sys.maxsize
-                    elif(i>=len(B)-2):
-                        if  B[i][1]==2:
-                            min_swap_sofar=B[i-1][1]
-                            current_neighbor_ix=i-2
+                            min_swap_sofar = sys.maxsize
+                    elif(i >= len(B)-2):
+                        if B[i][1] == 2:
+                            min_swap_sofar = B[i-1][1]
+                            current_neighbor_ix = i-2
                         else:
-                            min_swap_sofar=sys.maxsize
-                    else:# if in the middle not at first and end
-                        min_swap_sofar = min(B[i-1][1],B[i+1][1]) if B[i][1]==2 else (B[i-1][1]+B[i+1][1])
-                        if B[i+1][1]<B[i-1][1]:
-                            current_neighbor_ix=i+2
-                        elif(B[i+1][1]>B[i-1][1]):
-                            current_neighbor_ix=i-2
+                            min_swap_sofar = sys.maxsize
+                    else:  # if in the middle not at first and end
+                        min_swap_sofar = min(
+                            B[i-1][1], B[i+1][1]) if B[i][1] == 2 else (B[i-1][1]+B[i+1][1])
+                        if B[i+1][1] < B[i-1][1]:
+                            current_neighbor_ix = i+2
+                        elif(B[i+1][1] > B[i-1][1]):
+                            current_neighbor_ix = i-2
                         else:
-                            if(B[i-2][1]<B[i+2][1]):# select the based on the size
-                                current_neighbor_ix=i-2
-                            elif B[i-2][1]>B[i+2][1]:
-                                current_neighbor_ix=i+2
-                            else: # if they are equal in size 
-                                if(B[i][1]==2): # if two aa or bb
-                                    if(i+2<len(B)-1):
-                                        current_neighbor_ix=i+2
+                            if(B[i-2][1] < B[i+2][1]):  # select the based on the size
+                                current_neighbor_ix = i-2
+                            elif B[i-2][1] > B[i+2][1]:
+                                current_neighbor_ix = i+2
+                            else:  # if they are equal in size
+                                if(B[i][1] == 2):  # if two aa or bb
+                                    if(i+2 < len(B)-1):
+                                        current_neighbor_ix = i+2
                                     else:
-                                        current_neighbor_ix=i-2
-                                else: # if just one a or b
-                                    if(i+2<len(B)-1):
-                                        current_neighbor_ix=i+2
-                                    elif(i-2>0):
-                                        current_neighbor_ix=i-2
-                                    else:# if both are move candidate to one side
-                                        current_neighbor_ix=i
+                                        current_neighbor_ix = i-2
+                                else:  # if just one a or b
+                                    if(i+2 < len(B)-1):
+                                        current_neighbor_ix = i+2
+                                    elif(i-2 > 0):
+                                        current_neighbor_ix = i-2
+                                    else:  # if both are move candidate to one side
+                                        current_neighbor_ix = i
                     if(min_swap_sofar < min_total_swap):
                         candidate_ix = i
-                        neighbor_ix=current_neighbor_ix
-                    elif min_swap_sofar == min_total_swap and min_swap_sofar!=sys.maxsize:
-                        if(B[neighbor_ix][1]>B[current_neighbor_ix][1]):
+                        neighbor_ix = current_neighbor_ix
+                    elif min_swap_sofar == min_total_swap and min_swap_sofar != sys.maxsize:
+                        if(B[neighbor_ix][1] > B[current_neighbor_ix][1]):
                             candidate_ix = i
-                            neighbor_ix=current_neighbor_ix
+                            neighbor_ix = current_neighbor_ix
                         else:
-                            pre_gap=getGap2(current_char,B,candidate_ix,neighbor_ix,ops_char_start_ix,ops_char_end_ix,ops_char_gap)
-                            cur_gap=getGap2(current_char,B,i,current_neighbor_ix,ops_char_start_ix,ops_char_end_ix,ops_char_gap)
-                            if(cur_gap<pre_gap):
+                            pre_gap = getGap2(
+                                current_char, B, candidate_ix, neighbor_ix, ops_char_start_ix, ops_char_end_ix, ops_char_gap)
+                            cur_gap = getGap2(
+                                current_char, B, i, current_neighbor_ix, ops_char_start_ix, ops_char_end_ix, ops_char_gap)
+                            if(cur_gap < pre_gap):
                                 candidate_ix = i
-                                neighbor_ix=current_neighbor_ix
+                                neighbor_ix = current_neighbor_ix
                     min_total_swap = min(min_swap_sofar, min_total_swap)
-            
+                i=max(neighbor_ix-1,candidate_ix+1,i)
+                i+=1
+                #print("i", i)
+
             #print("{} {} {}".format(candidate_ix,"<==" if candidate_ix<neighbor_ix else "= or ==>" ,neighbor_ix))
-                                
-            if(B[candidate_ix][1]==2):#if two a : aa
-                B[candidate_ix][1]+=1
-                B[neighbor_ix][1]-=1
-                swap_count+=B[candidate_ix+1][1] if (candidate_ix<neighbor_ix) else B[candidate_ix-1][1]
-            else:# if jus one a
-                # if(a_max_length < 3 and b_max_length < 3):
-                if(candidate_ix==neighbor_ix):
-                        B[candidate_ix-1][1]-=1
-                        B[candidate_ix+1][1]+=1
+
+            if(B[candidate_ix][1] == 2):  # if two a : aa
+                B[candidate_ix][1] += 1
+                B[neighbor_ix][1] -= 1
+                swap_count += B[candidate_ix+1][1] if (
+                    candidate_ix < neighbor_ix) else B[candidate_ix-1][1]
+            else:  # if jus one a
+                if(candidate_ix == neighbor_ix):
+                    B[candidate_ix-1][1] -= 1
+                    B[candidate_ix+1][1] += 1
                 else:
-                #     if(candidate_ix==neighbor_ix):
-                #         neighbor_ix=candidate_ix+2
-                #B[candidate_ix][1]+=1
-                    B[neighbor_ix][1]-=1
-                    if(candidate_ix<neighbor_ix):#change the right side
-                        B[candidate_ix+1][1]-=1
-                        B.insert(candidate_ix+2,[current_char,1])
-                        B.insert(candidate_ix+3,[reversChar(current_char),1])
-                    else:# change the left side
-                        B[candidate_ix-1][1]-=1
-                        B.insert(neighbor_ix+1,[reversChar(current_char),1])
-                        B.insert(neighbor_ix+2,[current_char,1])
-                swap_count+=1
+                    B[neighbor_ix][1] -= 1
+                    if(candidate_ix < neighbor_ix):  # change the right side
+                        B[candidate_ix+1][1] -= 1
+                        B.insert(candidate_ix+2, [current_char, 1])
+                        B.insert(candidate_ix+3, [reversChar(current_char), 1])
+                    else:  # change the left side
+                        B[candidate_ix-1][1] -= 1
+                        B.insert(neighbor_ix+1, [reversChar(current_char), 1])
+                        B.insert(neighbor_ix+2, [current_char, 1])
+                swap_count += 1
             B = [x for x in B if x[1] > 0]
             B_stack = []
             for i in B:
@@ -150,63 +158,61 @@ def solution(S):
     return swap_count
 
 
-
 def getGap1(chr, B):
     C = [x[0] for x in B]
     start = C.index(chr)
     end = len(C)-1-C[::-1].index(chr)
     return sum([x[1] for x in B[start:end] if x[0] != chr])
 
-def getGap2(current_char, D,candidate_ix,neighbor_ix,ops_char_start_ix,ops_char_end_ix,ops_char_gap):
-    D=copy.deepcopy(D)
-    if(D[candidate_ix][1]==2):#if two a : aa
-        D[candidate_ix][1]+=1
-        D[neighbor_ix][1]-=1
-    else:# if jus one a
-        if(candidate_ix==neighbor_ix):
-                D[candidate_ix-1][1]-=1
-                D[candidate_ix+1][1]+=1
+
+def getGap2(current_char, D, candidate_ix, neighbor_ix, ops_char_start_ix, ops_char_end_ix, ops_char_gap):
+    D = copy.deepcopy(D)
+    if(D[candidate_ix][1] == 2):  # if two a : aa
+        D[candidate_ix][1] += 1
+        D[neighbor_ix][1] -= 1
+    else:  # if jus one a
+        if(candidate_ix == neighbor_ix):
+            D[candidate_ix-1][1] -= 1
+            D[candidate_ix+1][1] += 1
         else:
-            D[neighbor_ix][1]-=1
-            if(candidate_ix<neighbor_ix):#change the right side
-                D[candidate_ix+1][1]-=1
-                D.insert(candidate_ix+2,[current_char,1])
-                D.insert(candidate_ix+3,[reversChar(current_char),1])
-            
-            else:# change the left side
-                D[candidate_ix-1][1]-=1
-                D.insert(neighbor_ix+1,[reversChar(current_char),1])
-                D.insert(neighbor_ix+2,[current_char,1])
-    if(candidate_ix not in range(ops_char_start_ix+1,ops_char_end_ix,1) and neighbor_ix in  range(ops_char_start_ix+1,ops_char_end_ix,1)):
-        ops_char_gap-=1
-    elif(candidate_ix in range(ops_char_start_ix+1,ops_char_end_ix,1) and neighbor_ix not in  range(ops_char_start_ix+1,ops_char_end_ix,1)):
-        ops_char_gap+=1
+            D[neighbor_ix][1] -= 1
+            if(candidate_ix < neighbor_ix):  # change the right side
+                D[candidate_ix+1][1] -= 1
+                D.insert(candidate_ix+2, [current_char, 1])
+                D.insert(candidate_ix+3, [reversChar(current_char), 1])
 
-    # C = [x[0] for x in D]
-    # start = C.index(reversChar(current_char))
-    # end = len(C)-1-C[::-1].index(reversChar(current_char))
-    # return sum([x[1] for x in D[start:end] if x[0] == current_char]),ops_char_gap
-    
+            else:  # change the left side
+                D[candidate_ix-1][1] -= 1
+                D.insert(neighbor_ix+1, [reversChar(current_char), 1])
+                D.insert(neighbor_ix+2, [current_char, 1])
+    if(not (ops_char_start_ix < candidate_ix and candidate_ix < ops_char_end_ix)
+       and (ops_char_start_ix < neighbor_ix and neighbor_ix < ops_char_end_ix)):
+        ops_char_gap -= 1
+    elif((ops_char_start_ix < candidate_ix and candidate_ix < ops_char_end_ix, 1) and not(ops_char_start_ix < neighbor_ix and neighbor_ix < ops_char_end_ix)):
+        ops_char_gap += 1
+    # if(candidate_ix not in range(ops_char_start_ix+1, ops_char_end_ix, 1) and neighbor_ix in range(ops_char_start_ix+1, ops_char_end_ix, 1)):
+    #     ops_char_gap -= 1
+    # elif(candidate_ix in range(ops_char_start_ix+1, ops_char_end_ix, 1) and neighbor_ix not in range(ops_char_start_ix+1, ops_char_end_ix, 1)):
+    #     ops_char_gap += 1
+
     return ops_char_gap
- 
 
 
-print(solution("aabbbbababa"))#2
-print("this:",solution("abbbbaa"))#4
-print(solution("baabaab"))#5
-print(solution("ababab"))#3
-print(solution("abaabaaba"))#5
-print(solution("abaabaaba"))#4
-print(solution("ababbaabba"))#2
-print(solution("bbabbaababaababb"))#1
-print(solution("aabaababbbbbabaa"))#1
-print(solution("abbaabba"))#5
-print(solution("ababbababb"))#3
-print(solution("bbabbababb"))#3
-print(solution("bbabbaababaababb"))#1
-print(solution("ababbababb"))#3
-print(solution("aababbababbaabaa"))#1
-print(solution("ababbababb"))#3
-print(solution("aababaabaa"))#3
-print(solution("ababbababb"))#3
-
+print(solution("aabbbbababa"),2)  # 2
+print( solution("abbbbaa"),4)  # 4
+print(solution("baabaab"),5)  # 5
+print(solution("ababab"),3)  # 3
+print(solution("abaabaaba"),4)  # 4
+print(solution("abaabaaba"),4)  # 4
+print(solution("ababbaabba"),2)  # 2
+print(solution("bbabbaababaababb"),1)  # 1
+print(solution("aabaababbbbbabaa"),1)  # 1
+print(solution("abbaabba"),5)  # 5
+print(solution("ababbababb"),3)  # 3
+print(solution("bbabbababb"),3)  # 3
+print(solution("bbabbaababaababb"),1)  # 1
+print(solution("ababbababb"),3)  # 3
+print(solution("aababbababbaabaa"),1)  # 1
+print(solution("ababbababb"),3)  # 3
+print(solution("aababaabaa"),3)  # 3
+print(solution("ababbababb"),3)  # 3
