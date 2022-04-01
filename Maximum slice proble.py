7fcad0cb7cc821120e20d17f55f65e0392006136
Mohammad Yahya Akhlaqi <91276692+myakhlaqi@users.noreply.github.com>
2022-04-01 23:58:38 +0800

    Update Maximum slice proble.py

--- Maximum slice proble.py
+++ Maximum slice proble.py
@@ -5,6 +5,6 @@ def golden_max_slice(A):
     for a in A:
         max_ending = max(0, max_ending + a)
         max_slice = max(max_slice, max_ending)
-    return max_slice
+    return max_slice 
 
 print(golden_max_slice([5,-7,3,5,-2,4,-1]))
