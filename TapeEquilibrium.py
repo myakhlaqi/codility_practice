567cad5afc4cf8aa7754c0076d2575774d3d2cbd
Mohammad Yahya Akhlaqi <91276692+myakhlaqi@users.noreply.github.com>
2022-04-06 01:33:46 +0800

    Update TapeEquilibrium.py

--- TapeEquilibrium.py
+++ TapeEquilibrium.py
@@ -5,7 +5,7 @@ def solution1(A):
     for part in range(1,len(A)):
         p1=A[:part]
         p2=A[part:]
-        dif_list.append(abs(sum(p1)-sum(p2)))
+        dif_list.append(abs(sum(p1)-sum(p2))) 
     
     return  min(dif_list,default=0)
         
