05763ee5e2d7038a0e722bceadb6d8c1d8a3e771
Mohammad Yahya Akhlaqi <91276692+myakhlaqi@users.noreply.github.com>
2022-04-07 23:47:35 +0800

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -1,6 +1,6 @@
 # Determine whether a triangle can be built from a given set of edges.
 
-def solution(A):
+def solution(A): 
     n=len(A)
     for i in range(n-2):
         for j in range(i+1,n-1):
