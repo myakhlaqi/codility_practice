63f76d4ba686e03f9c48990f8dfba8d3dba1e988
Mohammad Yahya Akhlaqi <91276692+myakhlaqi@users.noreply.github.com>
2022-07-04 23:38:45 +0800

    Update MaxDoubleSliceSum-9.py

--- MaxDoubleSliceSum-9.py
+++ MaxDoubleSliceSum-9.py
@@ -1,5 +1,5 @@
 #Find the maximal sum of any double slices.
-#https://app.codility.com/programmers/lessons/9-maximum_slice_problem/max_double_slice_sum/
+#https://app.codility.com/programmers/lessons/9-maximum_slice_problem/max_double_slice_sum/ 
 
 import sys
 
