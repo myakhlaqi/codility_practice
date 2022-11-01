ed025a12cd631918d05774438bf41d305b1ac774
Yahya <91276692+myakhlaqi@users.noreply.github.com>
2022-11-01 17:47:46 +0800

    Update PermMissingElem.py

--- PermMissingElem.py
+++ PermMissingElem.py
@@ -4,6 +4,6 @@ def solution(A):
     set_b={ * range(1,len(A)+2)} 
     # print("set_b", set_b)
     result=set_b - set_a
-    # print("result")
+    # print(result)
     return list(result)[0]
 # print(solution([2,3,1,5]))
